68f55e0d28d5e159104c6b7066fcec0418327414
Elizabethucar <elizg77@hotmail.com>
2022-09-29 19:46:01 +0200

    React app and form created

---
+++ leverans/src/App.js
@@ -0,0 +1,14 @@
+import Form from './Form'
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ leverans/src/Form.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import './App.css'
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+
+const Form = ()=>{
+ 
+
+const schema = yup.object().shape({
+  firstName: yup.string().required("Must have firstname"),
+  lastName: yup.string().required("Enter lastname"),
+  email: yup.string().email().required("Email is required"),
+  age: yup.number().positive("Must be a number"),
+  password: yup.string().min(8).max(15).required("Password is required"),
+  confirmPassword: yup.string().oneOf([yup.ref("password"), null]).required("Passwords must match"),
+})
+const {
+  register,
+  handleSubmit,
+  formState: { errors },
+  } = useForm({
+  resolver: yupResolver(schema),
+});
+
+  const onSubmit =(data)=>{
+    console.log(data);
+  }
+  return(
+
+<div className='Form'>
+  <div className='title'>Sign up</div>
+  <div className='inputs'>
+<form onSubmit={handleSubmit(onSubmit)}>
+
+  <input type="text" 
+   name='firstName'
+   placeholder='Firstname...'  
+   {...register("firstName")} />
+   <p>{errors.firstName?.message}</p>
+
+  <input type="text"
+   name='lastName'
+   placeholder='Lastname...'  
+   {...register("lastName")} />
+   <p>{errors.lastName?.message}</p>
+
+  <input type="text" 
+  name='email' 
+  placeholder='Email...'  
+  {...register("email")}/>
+<p>{errors.email?.message}</p>
+
+  <input type="number"
+   name='age' 
+   placeholder='Age..'  
+   {...register("age")} />
+ <p>{errors.age?.message}</p>
+
+  <input type="password" 
+  name='password' 
+  placeholder='Password...' 
+  {...register("password")}/>
+<p>{errors.password?.message}</p>
+
+  <input type="password"
+   name='confirmPassword'
+   placeholder='Confirm password...' 
+   {...register("confirmPassword")} />
+  <p>{errors.confirmPassword && 'Passwords should match!'}</p>
+
+   <input type="submit" id="submit"  />
+
+    </form>
+  </div>
+</div>
+
+  )
+}
+export default Form;
