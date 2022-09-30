5e7f5723f5a27466ec6c55859cbb5d6003837fa0
Sara Razm <razmsara@gmail.com>
2022-09-30 10:32:34 +0200

    Nodemon installed and applied

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = require('express').Router()
 const PORT = process.env.PORT || 3000
-
+const cors = require('cors');
 const app= express()
+
+app.use(cors());
 app.use = (express.json())
 
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+});
 
 app.listen(PORT, ()=>{
   console.log(`listening on port ${PORT}`)
