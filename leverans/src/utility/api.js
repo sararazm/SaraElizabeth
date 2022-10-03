64754595939166d0f0677b9df2b3cfd5ee440855
Sara Razm <razmsara@gmail.com>
2022-10-03 12:34:28 +0200

    add fetch api in frontend

---
+++ leverans/src/utility/api.js
@@ -0,0 +1,24 @@
+const fetchData = (url, method, body = {}) => {
+    const options = {
+        method,
+    }
+
+    if (method == 'POST' || method == 'PUT') {
+        options.headers = {
+            "content-Type": "application/json",
+        }
+        options.body = JSON.stringify(body);
+    }
+
+    return fetch(url, options).then((res) => res.json())
+};
+
+const get = (url) => fetchData(url, 'GET')
+
+const post = (url, body) => fetchData(url, 'POST', body)
+
+const put = (url, body) => fetchData(url, 'PUT', body)
+
+const taBort = (url) => fetchData(url, 'DELETE')
+
+export { get, put, taBort, post };
